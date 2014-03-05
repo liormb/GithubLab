36e7db95db897d6b4cbeae8866c293b0b13c1f95
liormb <liormb@gmail.com>
2014-03-04 19:48:00 -0500

    first init

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Githublab::Application.config.secret_key_base = 'b9fcdb49a03052e4a15060890dd5430e0c61299d9d8c07b74b19d36d880b16ae660cc971cd3ce88a8ea634fbfd1f685ba0341dd70450b0c27d68983b1836ebbe'
